fe64d7dd43d102fad2419a114b7ba42ef0e22ae7
studiopandora <kimitrimusic@gmail.com>
2024-09-17 23:50:53 -0300

    Set Cloudflare DNS in systemd-resolved and update WireGuard configuration

--- scripts/wireguard-installer.sh
+++ scripts/wireguard-installer.sh
@@ -3,6 +3,10 @@ dnf update && dnf upgrade -y
 
 dnf install wireguard-tools iptables cronie -y
 
+# Set DNS server to Cloudflare's DNS
+sed -i 's/^#DNS=/DNS=1.1.1.1/' /etc/systemd/resolved.conf
+sed -i 's/^#FallbackDNS=/FallbackDNS=1.1.1.1/' /etc/systemd/resolved.conf
+
 # Sets up a scheduled task to update and upgrade packages weekly
 systemctl start crond
 systemctl enable crond
@@ -11,9 +15,6 @@ crontab cronupdateweekly
 
 # Save keys to files
 cd /etc/wireguard/
-echo $SERVER_PRIVATE_KEY > /etc/wireguard/serverprivatekey
-echo $CLIENT_PUBLIC_KEY > /etc/wireguard/clientpublickey
-echo $CLIENT_PRESHARED_KEY > /etc/wireguard/clientpresharedkey
 
 # Retrieve the default network interface
 INTERFACE=$(ip route list default | awk '{print $5}')
@@ -21,22 +22,21 @@ INTERFACE=$(ip route list default | awk '{print $5}')
 # Create and write VPN configuration
 cat <<EOF > /etc/wireguard/wg0.conf
 [Interface]
-Address = 10.0.0.1/24
+Address = 10.0.1.0/24
 ListenPort = 51820
-PostUp = wg set wg0 private-key /etc/wireguard/serverprivatekey
-PostUp = iptables -A INPUT -i wg0 -j DROP; iptables -A INPUT -i wg0 -s 10.0.0.2 -j ACCEPT
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE
+PrivateKey = ${SERVER_PRIVATE_KEY} 
+PostUp = sysctl net.ipv4.ip_forward=1
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT
+PostUp = iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
+PostDown = sysctl net.ipv4.ip_forward=0
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT
+PostDown = iptables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE
 
 [Peer]
-PublicKey =$(cat /etc/wireguard/clientpublickey)   
-PresharedKey = $(cat /etc/wireguard/clientpresharedkey)
+PublicKey = ${CLIENT_PUBLIC_KEY}  
+PresharedKey = ${CLIENT_PRESHARED_KEY} 
 AllowedIPs = 10.0.0.2/32
 EOF
-
-# Enable IP forwarding in the system configuration
-echo "net.ipv4.ip_forward = 1" | tee -a /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
 
 # Start and enable the WireGuard service to run on boot
 systemctl start wg-quick@wg0
